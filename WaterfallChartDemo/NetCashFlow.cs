db62e03a3545d3ea91cb098226bec410bb75a483
VimalaThirumalaiKumarSF3739 <vimala.t@syncfusion.com>
2023-05-04 17:44:33 +0530

    Sample updated

---
+++ WaterfallChartDemo/NetCashFlow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaterfallChartDemo
+{
+    public class NetCashFlow
+    {
+        public NetCashFlow()
+        {
+            Data = new List<NetCashFlowModel>();
+            Data.Add(new NetCashFlowModel() { Month = "Jan", Fund = 2500 });
+            Data.Add(new NetCashFlowModel() { Month = "Feb", Fund = 2250 });
+            Data.Add(new NetCashFlowModel() { Month = "Mar", Fund = -1000 });
+            Data.Add(new NetCashFlowModel() { Month = "Apr", Fund = 2300 });
+            Data.Add(new NetCashFlowModel() { Month = "May", Fund = 700 });
+            Data.Add(new NetCashFlowModel() { Month = "Jun", Fund = -1500 });
+            Data.Add(new NetCashFlowModel() { Month = "Jul", Fund = -800 });
+            Data.Add(new NetCashFlowModel() { Month = "Aug", Fund = -600 });
+            Data.Add(new NetCashFlowModel() { Month = "Sep", Fund = -900 });
+            Data.Add(new NetCashFlowModel() { Month = "Oct", Fund = 2250 });
+            Data.Add(new NetCashFlowModel() { Month = "Nov", Fund = 1200 });
+            Data.Add(new NetCashFlowModel() { Month = "Dec", Fund = -3000 });
+            Data.Add(new NetCashFlowModel() { Month = "Total", IsSum = true });
+        }
+        public List<NetCashFlowModel> Data { get; set; }
+    }
+}
---
+++ WaterfallChartDemo/NetCashFlowModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaterfallChartDemo
+{
+    public class NetCashFlowModel
+    {
+        public string Month { get; set; }
+        public double Fund { get; set; }
+        private bool isSum = false;
+
+        public bool IsSum
+        {
+            get { return isSum; }
+            set { isSum = value; }
+        }
+    }
+}
